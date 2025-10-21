0b2dc4682bde13ee7aae374fcb8a8c3b8fa1b5c3
Kuda Laut <adisaga097@gmail.com>
2025-10-21 13:50:19 +0700

    Update script.js

--- script.js
+++ script.js
@@ -2,3 +2,9 @@ const tombolSapa = document.getElementById('sapaButton')
 tombolSapa.addEventListener('click', function(){
   alert('WASSUP DAWG!! THX already coming here');
 });
+
+toggleBtn.addEventListener("click", () => {
+      body.classList.toggle("night");
+      const isNight = body.classList.contains("night");
+      toggleBtn.textContent = isNight ? "🌙" : "☀️";
+    });
